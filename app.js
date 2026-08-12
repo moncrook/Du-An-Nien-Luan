a4ac4bb0008ba5ac26e3c009d8e9ed5687a4ffaf
Phan Xuan Nhan <xuannhanll123@gmail.com>
2026-08-12 09:59:08 +0700

    Demo CI/CD pipeline deployment

--- app.js
+++ app.js
@@ -22,8 +22,8 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.listen(3000, () => {
-  console.log('Server running on port 3000');
+app.listen(8088, () => {
+  console.log('Server running on port 8088');
 });
 
 module.exports = app;
