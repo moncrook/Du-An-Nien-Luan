644f94ecca7c929bbf97030442b53f72d3f52cb6
Phan Xuan Nhan <xuannhanll123@gmail.com>
2026-08-12 09:21:32 +0700

    Demo CI/CD pipeline deployment

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ app.use(cors()); // Tích hợp mô-đun CORS để xử lý lỗi chia sẻ tà
 app.use('/', routes);
 
 
-// app.listen(8088, () => { // 监听8088端口
-// 	console.log('服务已启动 http://localhost:8088');
-// })
+app.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
 
 module.exports = app;
